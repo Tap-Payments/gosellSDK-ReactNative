e0d5522bb74e84a91875b651a433434c4b184ee2
kareemTap <k.ahmed@tap.company>
2020-10-11 13:59:08 +0300

    1. Updated UI to match flutter UI. 2. Showing result and status. 3. Separated header component.

--- App.js
+++ App.js
@@ -7,14 +7,18 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, NativeModules, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, NativeModules, Button, Alert, Platform, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native';
+import Header from './components/Header';
 
 export default class App extends Component {
 
 
   constructor(props) {
     super(props);
-    this.state = { statusNow: "not started" };
+    this.state = {
+      statusNow: "not started",
+      result: ""
+    };
     this.changeState = this.changeState.bind(this);
     this.startSDK = this.startSDK.bind(this);
   }
@@ -109,18 +113,18 @@ export default class App extends Component {
       console.log(myString);
       var resultStr = String(status.sdk_result);
 
-      this.changeState(resultStr, () => { console.log('done') })
-
+      this.changeState(resultStr, myString, () => { console.log('done') })
 
     })
 
   }
 
-  changeState(newName, callback) {
+  changeState(newName, resultValue, callback) {
     console.log('the new value is' + newName);
     this.setState(
       {
         statusNow: newName,
+        result: resultValue
       },
       callback,
     );
@@ -128,26 +132,74 @@ export default class App extends Component {
 
 
   render() {
+    const statusbar = (Platform.OS == 'ios') ? <StatusBar backgroundColor="blue" barStyle="light-content" /> : <View />
     const { statusNow } = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>goSellSDK-ReactNative!!</Text>
-        <Text> Status is {statusNow}</Text>
-        <Button
-          onPress={this.startSDK}
-          title="Start Payment"
-          color="#FF6347"
-        />
-        {/* <Button title="Change state" onPress={() => this.changeState('my name here')} /> */}
-      </View>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.container}>
+          {statusbar}
+          <Header title="Plugin Example app" />
+          <Text style={styles.statusText}> Status: {statusNow}</Text>
+          <Text style={styles.resultText} >{this.state.result}</Text>
+          <View style={styles.bottom}>
+            <TouchableOpacity onPress={this.startSDK} >
+              <View style={styles.payButtonBg}>
+                <Text style={styles.payButtonText}>
+                  Start Payment
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
 }
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: '#000'
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  statusbar: {
+    height: 20
+  },
+  payButtonBg: {
+    alignItems: 'center',
+    backgroundColor: '#25cf1f',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 25,
+    position: 'absolute',
+    bottom: 0,
+    width: '90%',
+    marginLeft: '6%',
+    marginRight: '10%'
+  },
+  payButtonText: {
+    color: '#FFF',
+    fontSize: 20
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 36
+  },
+  statusText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 15,
+    width: '90%',
+    marginLeft: '6%',
+    marginRight: '10%'
+  }
 });
