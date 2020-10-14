87ff812c1c78feda24e2d60c30498dc7636e0995
kholdunn <k.naqawa@tap.company>
2020-10-14 11:34:19 -0400

    clean

--- android/app/src/main/java/com/gosellsdkreactnative/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.gosellsdkreactnative;
-
-public class Constants {
-    public static int ERROR_CODE_INVALID_CUSTOMER_ID=501;
-    public static int ERROR_CODE_INVALID_TRX_MODE=502;
-    public static int ERROR_CODE_BACKEND_UNKNOWN_ERROR=503;
-}
