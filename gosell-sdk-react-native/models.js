4320823b91eecdacfbbcf5f9268696fd28872e06
kareemTap <k.ahmed@tap.company>
2020-10-14 18:36:27 +0300

    - Updated models.

--- gosell-sdk-react-native/models.js
+++ gosell-sdk-react-native/models.js
@@ -3,11 +3,37 @@ const Languages = {
     AR: 'ar',
 };
 
+const PaymentTypes = {
+    ALL: 'PaymentType.ALL',
+    CARD: 'PaymentType.CARD',
+    WEB: 'PaymentType.WEB',
+    APPLE_PAY: 'PaymentType.APPLE_PAY'
+}
+
+const AllowedCadTypes = {
+    CREDIT: 'CREDIT',
+    DEBIT: 'DEBIT'
+}
+
+const TrxMode = {
+    PURCHASE: 'TransactionMode.PURCHASE',
+    AUTHORIZE_CAPTURE: 'TransactionMode.AUTHORIZE_CAPTURE',
+    SAVE_CARD: 'TransactionMode.SAVE_CARD',
+    TOKENIZE_CARD: 'TransactionMode.TOKENIZE_CARD'
+}
+
+const SDKMode = {
+    Sandbox: 'SDKMode.Sandbox',
+    Production: 'SDKMode.Production'
+}
 
 
 const config = {
     Languages: Languages,
-
+    PaymentTypes: PaymentTypes,
+    AllowedCadTypes: AllowedCadTypes,
+    TrxMode: TrxMode,
+    SDKMode: SDKMode
 }
 
 
